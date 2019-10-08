bb670fc161cac3fbce203acf11faf1f61e875f3e
hawkeyes1094 <juluruteja@gmail.com>
2019-10-09 01:18:16 +0530

    Added Algo Lab week 9 Q1 and B1 assessment question

--- AL/week_9/q1_horspool.c
+++ AL/week_9/q1_horspool.c
@@ -10,6 +10,9 @@
 #define TABLESIZE 256
 
 void BadChar(char* pattern,int size,int* shift_table) {
+	for(int i = 0;i < TABLESIZE;i++)
+		shift_table[i] = size;
+
 	for(int i = 0,j = size - 1;i < size,j >= 0;i++,j--) {
 		shift_table[(int)pattern[i]] = j;
 	}
@@ -37,35 +40,24 @@ int main(int argc, char const *argv[])
 	
 	BadChar(pattern,strlen(pattern),shift_table);
 	
-	int l,r,p;
-	l = pattern_len - 1;
-	r = l;
-	while(l < text_len) {
-		r = l;
-		p = pattern_len - 1;
-		while(text[r] == pattern[p]) {
-			r--;
-			p--;
-		}
-		if(p < 0) {
-			printf("Pattern found at index = %d\n",l - pattern_len + 1);
+	//
+	int i = pattern_len - 1;
+
+	while(i < text_len) {
+
+		int k = 0;
+		while(k < pattern_len && pattern[pattern_len-1-k] == text[i-k])
+			k++;
+		
+		if(k == pattern_len) {
+			printf("Pattern found at index = %d\n",i - pattern_len + 1);
 			break;
 		}
-		printf("l = %d,r = %d,p = %d\n",l,r,p);
-		int shift_len = shift_table[(int)text[r]];
-		if(shift_len == 0)
-			shift_len = pattern_len - 1;
-
-		printf("shifted by length = %d\n",shift_len);
-		l = l + shift_len;
+		i += shift_table[text[i]];
+	
 	}
-	if(l >= text_len)
+	if(i >= text_len)
 		printf("Pattern not found\n");
 
-	// for(int i = 0;i < TABLESIZE;i++) {
-	// 	if(shift_table[i] > 0)
-	// 		printf("%c -> %d ",(char)i,shift_table[i]);
-	// }
-	// printf("\n");
 	return 0;
 }
